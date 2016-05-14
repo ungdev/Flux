419da6f8a8c99e490b06bbbabbef3bef38d62aa0
nashe <thomas@chauchefoin.fr>
2016-05-14 21:56:33 +0200

    Fix config paths

--- lib/conf/config.php
+++ lib/conf/config.php
@@ -4,7 +4,7 @@ $config['site_name']  = 'FluxManager';     //[basic] Nom du portail
 $config['baseDir']  = '';       // [basic] Répertoire racine du site
 $config['baseUrl']  = 'http://'.$_SERVER['HTTP_HOST'].$config['baseDir']; // [basic] Url de base du site
 $config['tplUrl']  = $config['baseUrl'] .'/tpl/default';     // [basic] Template
-$config['baseServer']  = '/var/www';      // dossier où est stocké la racine du site
+$config['baseServer']  = dirname(__FILE__).'/../..';      // dossier où est stocké la racine du site
 $config['libServer']  = $config['baseServer'] .'/lib';     // [basic] Librairie
 $config['tplServer']  = $config['baseServer'] .'/tpl/default';     // [basic] Template
 
